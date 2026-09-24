908c064fb8306717fba92dc39c341cfd6454bdc6
JoseMC13 <ppmch2007@gmail.com>
2026-09-23 20:39:32 -0600

    cambio

--- Proyecto/Main.java
+++ Proyecto/Main.java
@@ -1,15 +1,5 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) {
-        new VistaJuego().mostrar(stage);
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        new ControladorJuego().iniciar();
     }
 }
-
-                
